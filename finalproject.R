fb20b91f83fa752cd58902772d3d3b501a163959
CameronZinn <czinn12345@gmail.com>
2021-11-30 11:57:56 -0400

    WHEYYY

--- finalproject.R
+++ finalproject.R
@@ -13,7 +13,7 @@ tree_data <-  read_xlsx("SMUtreedatabase2021.xlsx")
 project_data <- merge(class_data, tree_data, by = "uid_4826")
 campus_data <- filter(project_data, location == "SMU campus")
 campus_data %>% drop_na(crown_condition)
-
+campus_data
 # Reading shapefiles
 campus_buildings <- readOGR(dsn = 'C:/Users/jense/Desktop/ENVS 4826 - Data Science in the Environment/FinalProject/CampusMap_Background_SHP', layer = "CampusBuildings")
 greenspace <- readOGR(dsn = 'C:/Users/jense/Desktop/ENVS 4826 - Data Science in the Environment/FinalProject/CampusMap_Background_SHP', layer = "Greenspace")
